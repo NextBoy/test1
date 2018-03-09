fa92d523165ffc29c4e300d2e17fd6b71d006e9f
zhuozhongzhi <zhongzhi_z@gzleihou.cn>
2018-03-09 20:20:23 +0800

    2

--- sw.js
+++ sw.js
@@ -9,7 +9,8 @@ sw.addEventListener('install', (event) => {
                     './index.html',
                     './static/js/manifest.4b311f6f04d156464c82.js',
                     './static/js/vendor.26dc01fa29168a43c768.js',
-                    './static/js/app.f8a29dd30a33f24bce71.js'
+                    './static/js/app.f8a29dd30a33f24bce71.js',
+                    './static/css/app.c2fd0948b12fbbccda9b977d8043c6ef.css'
                 ])
             })
     )
